1daef5a47b740ff74ac3e6ea6f37fe096923335d
DiegoRodriguesDev <diegolp12@gmail.com>
2024-01-24 09:22:30 -0300

    First Commit; Entities layer

---
+++ src/entity/order.ts
@@ -0,0 +1,34 @@
+import OrderItem from "./order_item";
+
+export default class Order {
+    
+    private _id: string;
+    private _customerId: string;
+    private _items: OrderItem[];
+    private _total: number;
+
+    constructor(id: string, customerId: string, items: OrderItem[]) {
+        this._id = id;
+        this._customerId = customerId;
+        this._items = items;
+        this._total = this.total();
+        this.validate();
+    }
+    
+    total(): number {
+        return this._items.reduce((acc, item) => acc + item.orderItemTotal(), 0);
+    }
+
+    validate(): Boolean {
+        if (this._id.length === 0) {
+            throw new Error("Id is required");
+        }
+        if (this._customerId.length === 0) {
+            throw new Error("CustomerId is required");
+        }
+        if (this._items.length === 0) {
+            throw new Error("Items are required");
+        }
+        return true;
+    }
+}
